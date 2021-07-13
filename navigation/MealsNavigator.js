2e7d5d7f551dce0af3345aa8a960654c350bf503
Tanner Schmutte <tannerschmutte@gmail.com>
2021-07-13 15:57:46 -0700

    enter categories dummy data and pass data between screens

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -6,13 +6,22 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Colors from '../constants/Colors';
 
-const MealsNavigator = createStackNavigator({
-    Categories: CategoriesScreen,
-    CategoryMeals: {
-        screen: CategoryMealsScreen,
+const MealsNavigator = createStackNavigator(
+    {
+        Categories: CategoriesScreen,
+        CategoryMeals: CategoryMealsScreen,
+        MealDetail: MealDetailScreen,
     },
-    MealDetail: MealDetailScreen,
-});
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: Colors.primary,
+            },
+            headerTintColor: 'white',
+        },
+    }
+);
 
 export default createAppContainer(MealsNavigator);
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    FlatList,
+    StyleSheet,
+    TouchableOpacity,
+} from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
 
 const CategoriesScreen = (props) => {
-    return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen</Text>
-            <Button
-                title="Go to Meals"
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity
+                style={styles.gridItem}
                 onPress={() => {
-                    props.navigation.navigate('CategoryMeals');
+                    props.navigation.navigate('CategoryMeals', {
+                        categoryId: itemData.item.id,
+                    });
                 }}
-            />
-        </View>
+            >
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+    return (
+        <FlatList
+            data={CATEGORIES}
+            renderItem={renderGridItem}
+            numColumns={2}
+        />
     );
+};
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
 };
 
 const styles = StyleSheet.create({
@@ -20,6 +45,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 100,
     },
 });
 
