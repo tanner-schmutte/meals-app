78a6a79d49d1eac66eb29c264ad902505ffd9ad4
Tanner Schmutte <tannerschmutte@gmail.com>
2021-07-14 09:38:52 -0700

    build MealItem component

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = (props) => {
+    const renderMealItem = (itemData) => {
+        return (
+            <MealItem
+                title={itemData.item.title}
+                image={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability}
+                onSelectMeal={() => {}}
+            />
+        );
+    };
+
     const categoryId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find(
-        (category) => category.id === categoryId
+    const displayedMeals = MEALS.filter(
+        (meal) => meal.categoryIds.indexOf(categoryId) >= 0
     );
 
     return (
         <View style={styles.screen}>
-            <Text>The Category Meal Screen</Text>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item, index) => {
+                    item.id;
+                }}
+                renderItem={renderMealItem}
+                style={{ width: '100%' }}
+            />
             <Button
                 title="Go to Details"
                 onPress={() => {
@@ -38,6 +59,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 10,
     },
 });
 
