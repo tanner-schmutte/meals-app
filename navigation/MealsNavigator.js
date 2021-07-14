7ac80c325fe499abeed9457d5800fbfda31b0742
Tanner Schmutte <tannerschmutte@gmail.com>
2021-07-14 12:34:58 -0700

    wrap MealsFavTabNavigator around MealsNavigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -6,6 +6,7 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen'
 import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator(
@@ -24,4 +25,11 @@ const MealsNavigator = createStackNavigator(
     }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+    {
+        Meals: MealsNavigator,
+        Favorites: FavoritesScreen
+    }
+)
+
+export default createAppContainer(MealsFavTabNavigator);
