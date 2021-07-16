2ba1fb4f1efdfcc7fb9722706c69230e6c3710db
Tanner Schmutte <tannerschmutte@gmail.com>
2021-07-16 10:14:42 -0700

    render fallback text when no favorites have been added

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import CustomHeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = (props) => {
     const favMeals = useSelector((state) => state.meals.favoriteMeals);
+
+    if (favMeals.length === 0 || !favMeals) {
+        return (
+            <View style={styles.content}>
+                <DefaultText>
+                    No favorite meals found. Start adding some!
+                </DefaultText>
+            </View>
+        );
+    }
 
     return <MealList listData={favMeals} navigation={props.navigation} />;
 };
@@ -27,5 +39,13 @@ FavoritesScreen.navigationOptions = (navData) => {
         ),
     };
 };
+ 
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default FavoritesScreen;
